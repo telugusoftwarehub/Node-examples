553181e662131c1fe6c2ae36a027e59de2997546
telugusoftwarehub <99103815+telugusoftwarehub@users.noreply.github.com>
2024-06-26 17:47:51 +0530

    Add files via upload

--- async await/app.js
+++ async await/app.js
@@ -1,1 +1,12 @@
+const fetchData = async () => {
+    try{
+        const data = await fetch('https://jsonplaceholder.typicode.com/users');
+        const json = await data.json();
+        console.log(json);
 
+    }catch(error){
+        console.error('Error fetching data:',error);
+    }
+
+};
+fetchData();
