fb8fbe8c76c79f5468c17a52deb820fa60f47d13
telugusoftwarehub <99103815+telugusoftwarehub@users.noreply.github.com>
2024-06-27 18:07:20 +0530

    Add files via upload

--- REST API/app.js
+++ REST API/app.js
@@ -1,1 +1,8 @@
-
+const express = require('express');
+const app = express();
+app.get('/', (req,res) => {
+    res.json({message:'Hello, API!'});
+});
+app.listen(3000, () => {
+    console.log('API running on port 3000');
+});
